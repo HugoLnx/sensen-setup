367e1ec5f2c4d57deb202165fdd3e9b13520802b
hugolnx <hugolnx@gmail.com>
2023-06-12 12:32:51 -0300

    Putting submodules folder at Assets/Standard Assets/

--- setup.sh
+++ setup.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
-SETUP_ROOT=$(dirname -- "$0")
-PROJECT_ROOT=$SETUP_ROOT/..
-BKP_FOLDER=$SETUP_ROOT/bkp/
+SETUP_ROOT="$(dirname -- "$0")"
+PROJECT_ROOT="$SETUP_ROOT/.."
+BKP_FOLDER="$SETUP_ROOT/bkp/"
+SUBMODULES_FOLDER="$PROJECT_ROOT/Assets/Standard Assets/Submodules"
 
-cd $PROJECT_ROOT
+cd "$PROJECT_ROOT"
 
 # BACKUP CONFIG FILES
 timestamp=$(date +%Y%m%d%H%M%S)
-mkdir -p $BKP_FOLDER
+mkdir -p "$BKP_FOLDER"
 
 function backupFileIfExists() {
-    filename=$1
+    filename="$1"
     # get second argument or default to "."
-    filefolder=${2:-"."}
-    filepath=$filefolder/$1
-    if [ -f $filepath ];then
+    filefolder="${2:-"."}"
+    filepath="$filefolder/$1"
+    if [ -f "$filepath" ];then
         echo "Backing up $filename in ./sensen-setup/bkp/"
-        cp -rf $filepath $BKP_FOLDER/${timestamp}_$filename
+        cp -rf "$filepath" "$BKP_FOLDER/${timestamp}_$filename"
     fi
 }
 
@@ -28,14 +29,14 @@ backupFileIfExists "packages.config" "Assets/"
 backupFileIfExists "manifest.json" "Packages/"
 
 # REPLACE CONFIG FILES
-cp $SETUP_ROOT/ConfigFiles/.gitignore .
+cp "$SETUP_ROOT/ConfigFiles/.gitignore" .
 if [[ -z "${ONLY_GIT}" ]]; then
-    cp $SETUP_ROOT/ConfigFiles/.editorconfig .
-    cp $SETUP_ROOT/ConfigFiles/omnisharp.json .
-    cp $SETUP_ROOT/ConfigFiles/NuGet.config ./Assets/
-    cp $SETUP_ROOT/ConfigFiles/packages.config ./Assets/
-    cp $SETUP_ROOT/ConfigFiles/manifest.json ./Packages/
-    cp -r $SETUP_ROOT/Templates/ProjectStructure/ ./Assets/MY_GAME_NAME/
+    cp "$SETUP_ROOT/ConfigFiles/.editorconfig" .
+    cp "$SETUP_ROOT/ConfigFiles/omnisharp.json" .
+    cp "$SETUP_ROOT/ConfigFiles/NuGet.config" ./Assets/
+    cp "$SETUP_ROOT/ConfigFiles/packages.config" ./Assets/
+    cp "$SETUP_ROOT/ConfigFiles/manifest.json" ./Packages/
+    cp -r "$SETUP_ROOT/Templates/ProjectStructure/" ./Assets/MY_GAME_NAME/
     mkdir ./PackagesBatch/
     touch ./PackagesBatch/.gitkeep
     mkdir ./Assets/Vendor/
@@ -44,11 +45,12 @@ fi
 
 git init
 if [[ -z "${ONLY_GIT}" ]]; then
+    mkdir -p "$SUBMODULES_FOLDER"
     git submodule init
-    git submodule add https://github.com/HugoLnx/unity-lnx-arch.git Assets/Submodules/unity-lnx-arch
-    git submodule add https://github.com/HugoLnx/unity-sensen-toolkit.git Assets/Submodules/unity-sensen-toolkit
-    git submodule add https://github.com/HugoLnx/unity-sensen-components.git Assets/Submodules/unity-sensen-components
-    git submodule add https://github.com/HugoLnx/unity-sensen-state-machine.git Assets/Submodules/unity-sensen-state-machine
+    git submodule add https://github.com/HugoLnx/unity-lnx-arch.git "$SUBMODULES_FOLDER/unity-lnx-arch"
+    git submodule add https://github.com/HugoLnx/unity-sensen-toolkit.git "$SUBMODULES_FOLDER/unity-sensen-toolkit"
+    git submodule add https://github.com/HugoLnx/unity-sensen-components.git "$SUBMODULES_FOLDER/unity-sensen-components"
+    git submodule add https://github.com/HugoLnx/unity-sensen-state-machine.git "$SUBMODULES_FOLDER/unity-sensen-state-machine"
     git submodule update --recursive --remote
     echo "Full setup finished."
 else
