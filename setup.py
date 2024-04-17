40bdcf8fc75de3fa665293a4cd84e7532b3f71db
hugolnx <hugolnx@gmail.com>
2024-04-17 09:05:53 +0200

    Refactoring: Extracting execution logic to SetupExecutor class

--- setup.py
+++ setup.py
@@ -1,26 +1,7 @@
-#!/usr/bin/env python3
-import os
-import glob
-from datetime import datetime
 import argparse
-import shutil
-import subprocess
-from src.manifest import merge_manifests, is_2d_manifest, is_mobile_manifest
-from src.nuget import merge_nuget_packages_config, update_omnisharp_json
-from src.utils import write_unix
 
-SETUP_ROOT = os.path.dirname(os.path.abspath(__file__))
-PROJECT_ROOT = os.path.abspath(os.path.join(SETUP_ROOT, '..'))
-BKP_FOLDER = os.path.join(SETUP_ROOT, 'bkp')
-SUBMODULES_FOLDER = os.path.join(PROJECT_ROOT, 'Assets', 'Plugins', 'Submodules')
-DEFAULT_PROJECT_NAME = os.path.basename(PROJECT_ROOT)
-
-OMNISHARP_SETUP_PATH = os.path.join(SETUP_ROOT, 'ConfigFiles', 'omnisharp.json')
-
-MANIFEST_PROJECT_PATH = os.path.join(PROJECT_ROOT, 'Packages', 'manifest.json')
-MANIFEST_SETUP_PATH = os.path.join(SETUP_ROOT, 'ConfigFiles', 'manifest.json')
-NUGET_PROJECT_PATH = os.path.join(PROJECT_ROOT, 'Assets', 'packages.config')
-NUGET_SETUP_PATH = os.path.join(SETUP_ROOT, 'ConfigFiles', 'packages.config')
+from src.SetupExecutor import SetupExecutor, MANIFEST_PROJECT_PATH
+from src.manifest import is_2d_manifest, is_mobile_manifest
 
 COMMAND_INIT_GIT = 'init-git'
 COMMAND_SETUP_INIT = 'init'
@@ -43,220 +24,80 @@ COMMANDS = [
     COMMAND_PUSH_NUGET,
 ]
 
-# BACKUP CONFIG FILES
-def backup_file_if_exists(filename, filefolder='.'):
-    filepath = os.path.join(filefolder, filename)
-    if os.path.isfile(filepath):
-        print(f'Backing up {filename} in bkp/')
-        timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-        shutil.copy(filepath, os.path.join(BKP_FOLDER, f'{timestamp}_{filename}'))
+if __name__ != '__main__':
+    throw('This script should not be imported')
 
-def backup_config_files():
-    backup_file_if_exists('.gitignore')
-    backup_file_if_exists('.gitattributes')
-    backup_file_if_exists('.editorconfig')
-    backup_file_if_exists('omnisharp.json')
-    backup_file_if_exists('NuGet.config', 'Assets')
-    backup_file_if_exists('packages.config', 'Assets')
-    backup_file_if_exists('manifest.json', 'Packages')
+parser = argparse.ArgumentParser()
+parser.add_argument('command', choices=COMMANDS, default=COMMANDS[0], help='Command to run', type=str)
+parser.add_argument('--name', default=None, help='Name of the game', type=str, required=False)
+parser.add_argument('--2d', dest='two_d', default=False, help='Force to use 2D dependencies', type=bool, required=False)
+parser.add_argument('--3d', dest='three_d', default=False, help='Force to use 3D dependencies', type=bool, required=False)
+parser.add_argument('--mobile', default=False, help='Use mobile dependencies', type=bool, required=False)
+parser.add_argument('--desktop', default=False, help='Use desktop dependencies', type=bool, required=False)
+parser.add_argument('--minimal', default=False, help='Remove some optional dependencies', type=bool, required=False)
+args = parser.parse_args()
 
-def init_git():
-    __replace_config('.gitignore', './')
-    __replace_config('gitattributes', './.gitattributes')
-    subprocess.run(['git', 'init'], cwd=PROJECT_ROOT)
-    subprocess.run(['git', 'lfs', 'install'], cwd=PROJECT_ROOT)
+if args.two_d and args.three_d:
+    print('Cannot use 2D and 3D at the same time')
+    exit(1)
 
-def import_configs():
-    push_nuget()
-    __replace_config('.editorconfig', '.')
-    __copy_all_files('PackagesBatch/*.unitypackage', './PackagesBatch/')
-
-def create_project_structure():
-    project_structure_path = os.path.join(PROJECT_ROOT, 'Assets', args.name)
-    if os.path.exists(project_structure_path):
-        print(f'!!! Project structure folder already exists: {project_structure_path}')
-    else:
-        shutil.copytree(os.path.join(SETUP_ROOT, 'Templates/ProjectStructure/'), project_structure_path)
-    __ensure_folder('Assets/Vendor')
-    __ensure_folder('Assets/Plugins/_Ignore')
-
-def init_submodules(only_toolkit=False):
-    os.makedirs(SUBMODULES_FOLDER, exist_ok=True)
-    subprocess.run(['git', 'submodule', 'init'], cwd=PROJECT_ROOT)
-    if not only_toolkit:
-        __add_submodule('dev', 'https://github.com/HugoLnx/unity-lnx-arch.git', 'unity-lnx-arch')
-        __add_submodule('dev', 'https://github.com/HugoLnx/unity-sensen-components.git', 'unity-sensen-components')
-    __add_submodule('dev', 'https://github.com/HugoLnx/unity-sensen-toolkit.git', 'unity-sensen-toolkit')
-    subprocess.run(['git', 'submodule', 'update', '--recursive', '--remote'], cwd=PROJECT_ROOT)
-
-def cleanup_submodules():
-    subprocess.run(['git', 'submodule', 'deinit', '-f', '.'], cwd=PROJECT_ROOT)
-    shutil.rmtree(SUBMODULES_FOLDER, ignore_errors=True)
-
-def pull_manifest():
-    result = merge_manifests(
-        source_path=MANIFEST_PROJECT_PATH,
-        target_path=MANIFEST_SETUP_PATH,
-        add_new_dependencies=False
-    )
-    new_manifest = result['new_manifest']
-    new_dependencies_snippet = result['new_dependencies_snippet']
-    version_updates_snippet = result['version_updates_snippet']
-
-    if version_updates_snippet is not None:
-        write_unix(MANIFEST_SETUP_PATH, new_manifest)
-        print('Updated setup base manifest...')
-        print(version_updates_snippet)
-    else:
-        print('Setup manifest has no versions to update')
-
-    if new_dependencies_snippet is not None:
-        print('> Project manifest has dependencies that are not in source manifest...')
-        print(new_dependencies_snippet)
-        print('Consider adding them manually to the setup manifest')
-
-def push_manifest(manifest_filters):
-    result = merge_manifests(
-        source_path=MANIFEST_SETUP_PATH,
-        target_path=MANIFEST_PROJECT_PATH,
-        add_new_dependencies=True,
-        filters=manifest_filters
-    )
-    new_manifest = result['new_manifest']
-    new_dependencies_snippet = result['new_dependencies_snippet']
-    version_updates_snippet = result['version_updates_snippet']
-    removed_dependencies = result['removed_dependencies']
-
-    has_updates = version_updates_snippet is not None \
-        or new_dependencies_snippet is not None \
-        or len(removed_dependencies) > 0
-
-    if has_updates:
-        write_unix(MANIFEST_PROJECT_PATH, new_manifest)
-        print('> Updated project manifest...')
-        print(version_updates_snippet)
-    else:
-        print('Project manifest is already up-to-date')
-
-    if new_dependencies_snippet is not None:
-        print('> Added new dependencies to project manifest')
-        print(new_dependencies_snippet)
-
-    if len(removed_dependencies) > 0:
-        print('> Removed dependencies from project manifest')
-        print('\n'.join(removed_dependencies))
-
-def pull_nuget():
-    (new_content, packages) = merge_nuget_packages_config(
-        source_path=NUGET_PROJECT_PATH,
-        target_path=NUGET_SETUP_PATH,
-    )
-
-    write_unix(NUGET_SETUP_PATH, new_content)
-
-    new_json_content = update_omnisharp_json(OMNISHARP_SETUP_PATH, packages)
-    write_unix(OMNISHARP_SETUP_PATH, new_json_content)
-
-def push_nuget():
-    __replace_config('omnisharp.json', '.')
-    __replace_config('NuGet.config', 'Assets')
-    __replace_config('packages.config', 'Assets')
+if args.mobile and args.desktop:
+    print('Cannot use mobile and desktop at the same time')
+    print('PS.: If you are making a cross-platform game, just use --mobile to add the mobile dependencies')
+    exit(1)
 
 
-def __add_submodule(branch_name, git_url, folder_name):
-    folder_path = os.path.join(SUBMODULES_FOLDER, folder_name)
-    folder_relpath = os.path.relpath(folder_path, PROJECT_ROOT)
-    cmd = ['git', 'submodule', 'add', '-f', '-b', branch_name, '--', git_url, folder_relpath]
-    subprocess.run(cmd, cwd=PROJECT_ROOT)
+is_2d = args.two_d or (not(args.three_d) and is_2d_manifest(MANIFEST_PROJECT_PATH))
+is_3d = not is_2d
+is_mobile = args.mobile or (not(args.desktop) and is_mobile_manifest(MANIFEST_PROJECT_PATH))
+is_minimal = args.minimal
 
-def __replace_config(config_relative_path, target_relative_path):
-    config_path = os.path.join(SETUP_ROOT, 'ConfigFiles', config_relative_path)
-    target_path = os.path.join(PROJECT_ROOT, target_relative_path)
-    shutil.copy(config_path, target_path)
+was_auto_detected = not(args.two_d) and not(args.three_d)
+print(f'Using {'2D' if is_2d else '3D'}{' mobile' if is_mobile else ''}{' minimal' if is_minimal else ''} dependencies {'(Auto-detected)' if was_auto_detected else ''}')
 
-def __copy_all_files(source_folder, target_folder):
-    source_path = os.path.join(SETUP_ROOT, source_folder)
-    target_path = os.path.abspath(os.path.join(PROJECT_ROOT, target_folder))
-    os.makedirs(target_path, exist_ok=True)
+manifest_filters = {
+    '2d': is_2d,
+    '3d': is_3d,
+    'mobile': is_mobile,
+    'minimal': is_minimal,
+}
 
-    source_files = glob.glob(source_path)
-    for source_file in source_files:
-        if os.path.isfile(source_file):
-            shutil.copy(source_file, target_path)
-            print(f'Copied {source_file} to {target_path}')
+if args.command not in COMMANDS:
+    print(f'Invalid command: {args.command}')
+    print(f'Valid commands: {COMMANDS}')
+    exit(1)
 
-def __ensure_folder(folder_relative_path):
-    folder_path = os.path.join(PROJECT_ROOT, folder_relative_path)
-    os.makedirs(folder_path, exist_ok=True)
-    open(os.path.join(folder_path, '.gitkeep'), 'a').close()
+e = SetupExecutor(
+    project_name=args.name,
+    manifest_filters=manifest_filters,
+)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('command', choices=COMMANDS, default=COMMANDS[0], help='Command to run', type=str)
-    parser.add_argument('--name', default=DEFAULT_PROJECT_NAME, help='Name of the game', type=str, required=False)
-    parser.add_argument('--2d', dest='two_d', default=False, help='Force to use 2D dependencies', type=bool, required=False)
-    parser.add_argument('--3d', dest='three_d', default=False, help='Force to use 3D dependencies', type=bool, required=False)
-    parser.add_argument('--mobile', default=False, help='Use mobile dependencies', type=bool, required=False)
-    parser.add_argument('--desktop', default=False, help='Use desktop dependencies', type=bool, required=False)
-    parser.add_argument('--minimal', default=False, help='Remove some optional dependencies', type=bool, required=False)
-    args = parser.parse_args()
-
-    if args.two_d and args.three_d:
-        print('Cannot use 2D and 3D at the same time')
-        exit(1)
-
-    if args.mobile and args.desktop:
-        print('Cannot use mobile and desktop at the same time')
-        print('PS.: If you are making a cross-platform game, just use --mobile to add the mobile dependencies')
-        exit(1)
-
-
-    is_2d = args.two_d or (not(args.three_d) and is_2d_manifest(MANIFEST_PROJECT_PATH))
-    is_3d = not is_2d
-    is_mobile = args.mobile or (not(args.desktop) and is_mobile_manifest(MANIFEST_PROJECT_PATH))
-    is_minimal = args.minimal
-
-    was_auto_detected = not(args.two_d) and not(args.three_d)
-    print(f'Using {'2D' if is_2d else '3D'}{' mobile' if is_mobile else ''}{' minimal' if is_minimal else ''} dependencies {'(Auto-detected)' if was_auto_detected else ''}')
-
-    manifest_filters = {
-        '2d': is_2d,
-        '3d': is_3d,
-        'mobile': is_mobile,
-        'minimal': is_minimal,
-    }
-
-    if args.command not in COMMANDS:
-        print(f'Invalid command: {args.command}')
-        print(f'Valid commands: {COMMANDS}')
-        exit(1)
-
-    if args.command == COMMAND_INIT_GIT:
-        backup_config_files()
-        init_git()
-    elif args.command == COMMAND_SETUP_INIT:
-        backup_config_files()
-        init_git()
-        push_manifest(manifest_filters)
-        import_configs()
-        init_submodules(only_toolkit=True)
-    elif args.command == COMMAND_SETUP_ALL:
-        backup_config_files()
-        init_git()
-        push_manifest(manifest_filters)
-        import_configs()
-        create_project_structure()
-        init_submodules()
-    elif args.command == COMMAND_INIT_SUBMODULES:
-        init_git()
-        init_submodules()
-    elif args.command == COMMAND_RM_SUBMODULES:
-        cleanup_submodules()
-    elif args.command == COMMAND_PULL_MANIFEST:
-        pull_manifest()
-    elif args.command == COMMAND_PUSH_MANIFEST:
-        push_manifest(manifest_filters)
-    elif args.command == COMMAND_PULL_NUGET:
-        pull_nuget()
-    elif args.command == COMMAND_PUSH_NUGET:
-        push_nuget()
+if args.command == COMMAND_INIT_GIT:
+    e.backup_config_files()
+    e.init_git()
+elif args.command == COMMAND_SETUP_INIT:
+    e.backup_config_files()
+    e.init_git()
+    e.push_manifest()
+    e.import_configs()
+    e.init_submodules(only_toolkit=True)
+elif args.command == COMMAND_SETUP_ALL:
+    e.backup_config_files()
+    e.init_git()
+    e.push_manifest()
+    e.import_configs()
+    e.create_project_structure()
+    e.init_submodules()
+elif args.command == COMMAND_INIT_SUBMODULES:
+    e.init_git()
+    e.init_submodules()
+elif args.command == COMMAND_RM_SUBMODULES:
+    e.cleanup_submodules()
+elif args.command == COMMAND_PULL_MANIFEST:
+    e.pull_manifest()
+elif args.command == COMMAND_PUSH_MANIFEST:
+    e.push_manifest()
+elif args.command == COMMAND_PULL_NUGET:
+    e.pull_nuget()
+elif args.command == COMMAND_PUSH_NUGET:
+    e.push_nuget()
